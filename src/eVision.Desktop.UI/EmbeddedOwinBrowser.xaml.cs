5f57567eb372f5483cd1b0794776a9a0782b618d
Damian Hickey <dhickey@gmail.com>
2014-05-02 16:51:48 +0200

    Import from Fastnet

---
+++ src/eVision.Desktop.UI/EmbeddedOwinBrowser.xaml.cs
@@ -0,0 +1,84 @@
+﻿namespace eVision.Desktop.UI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Input;
+    using CefSharp;
+    using CefSharp.Wpf;
+
+    /// <summary>
+    ///     Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class EmbeddedOwinBrowser
+    {
+        private readonly Dictionary<string, Action> _propertyChangeHandlers = new Dictionary<string, Action>();
+        private readonly WebView _webView;
+
+        public EmbeddedOwinBrowser()
+        {
+            InitializeComponent();
+
+            var browserSettings = new BrowserSettings
+            {
+                ApplicationCacheDisabled = true,
+                PageCacheDisabled = true,
+                DatabasesDisabled = true,
+            };
+            _webView = new WebView("http://www.evision-software.com/", browserSettings);
+            _propertyChangeHandlers.Add("Address", () => AddressTextBox.Text = _webView.Address);
+            _webView.PropertyChanged += (sender, args) =>
+            {
+                Action handler;
+                if(_propertyChangeHandlers.TryGetValue(args.PropertyName, out handler))
+                {
+                    Dispatcher.Invoke(handler);
+                }
+            };
+            DockPanel.Children.Add(_webView);
+        }
+
+        public static void Init(Func<IDictionary<string, object>, Task> appFunc)
+        {
+            var settings = new Settings();
+            if(CEF.Initialize(settings))
+            {
+                // Plug in custom scheme handler here.
+                //CEF.RegisterScheme("http", new OwinSchemeHandlerFactory(appFunc));
+            }
+        }
+
+        private void AddressTextBox_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter)
+            {
+                return;
+            }
+            _webView.Load(AddressTextBox.Text);
+        }
+
+        private void ForwardButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            _webView.Forward();
+        }
+
+        private void BackButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            _webView.Back();
+        }
+
+        private void EmbeddedOwinBrowser_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F5)
+            {
+                _webView.Reload(true);
+                return;
+            }
+            if (e.Key == Key.F12)
+            {
+                _webView.ShowDevTools();
+            }
+        }
+    }
+}
---
+++ src/eVision.Desktop.UI/OwinSchemeHandler.cs
@@ -0,0 +1,88 @@
+﻿namespace eVision.Desktop.UI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using CefSharp;
+
+    internal class OwinSchemeHandler : ISchemeHandler
+    {
+        private readonly Func<IDictionary<string, object>, Task> _appFunc;
+
+        private static readonly Dictionary<int, string> ReasonPhrases = new Dictionary<int, string>()
+        {
+            { 200, "OK"},
+            { 301, "Moved Permanently"},
+            { 304, "Not Modified"},
+            { 404, "Not Found"}
+        }; 
+
+        public OwinSchemeHandler(Func<IDictionary<string, object>, Task> appFunc)
+        {
+            _appFunc = appFunc;
+        }
+
+        public bool ProcessRequestAsync(
+            IRequest request,
+            SchemeHandlerResponse response,
+            OnRequestCompletedHandler requestCompletedCallback)
+        {
+            Console.WriteLine(DateTime.UtcNow + ": " + request.Url);
+            IDictionary<string, string> headers = request.GetHeaders();
+            Dictionary<string, string[]> requestHeaders = headers.ToDictionary(header => header.Key, header => new[] {header.Value});
+            Stream requestBody = Stream.Null;
+            if(request.Body != null)
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(request.Body);
+                requestBody = new MemoryStream(bytes, 0, bytes.Length);
+            }
+            var stream = new MemoryStream();
+            var uri = new Uri(request.Url);
+            var environment = new Dictionary<string, object>
+            {
+                {"owin.RequestBody", requestBody},
+                {"owin.RequestHeaders", requestHeaders},
+                {"owin.RequestMethod", request.Method},
+                {"owin.RequestPath", uri.AbsolutePath},
+                {"owin.RequestPathBase", "/"},
+                {"owin.RequestProtocol", "HTTP/1.1"},
+                {"owin.RequestQueryString", uri.Query},
+                {"owin.RequestScheme", "HTTP/1.1"},
+                {"owin.ResponseBody", stream},
+                {"owin.ResponseHeaders", new Dictionary<string, string[]>()},
+            };
+            // Yucky continuation
+            _appFunc.Invoke(environment).ContinueWith(task =>
+            {
+                string status = "200 OK";
+                if(environment.ContainsKey("owin.ResponseStatusCode"))
+                {
+                    var statusCode = environment["owin.ResponseStatusCode"].ToString();
+                    status = environment.ContainsKey("owin.ResponseReasonPhrase")
+                        ? statusCode + " " + environment["owin.ResponseReasonPhrase"].ToString()
+                        : statusCode + " " + ReasonPhrases[int.Parse(environment["owin.ResponseStatusCode"].ToString())];
+                }
+                //TODO CefSharp seems to be ignoring the status code and turning it to a 200OK :|
+                response.ResponseHeaders = new Dictionary<string, string>
+                {
+                    {
+                        "Status Code", status
+                    }
+                };
+                var responseHeaders = (Dictionary<string, string[]>)environment["owin.ResponseHeaders"];
+                foreach(KeyValuePair<string, string[]> responseHeader in responseHeaders)
+                {
+                    response.ResponseHeaders.Add(responseHeader.Key, string.Join(";", responseHeader.Value));
+                }
+                response.MimeType = !response.ResponseHeaders.ContainsKey("Content-Type") ? "text/plain" : response.ResponseHeaders["Content-Type"];
+                response.ResponseStream = stream;
+                stream.Position = 0;
+                requestCompletedCallback();
+            });
+            return true;
+        }
+    }
+}
